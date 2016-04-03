f57c7b3a5a2572ccf8d4d5d708766c515b601adf
Matthew J. Berger <matthewberger@nevada.unr.edu>
2016-04-03 02:13:03 -0700

    Remove extra comments from VBO class.

--- Source/Core/VBO.cpp
+++ Source/Core/VBO.cpp
@@ -2,7 +2,6 @@
 
 VBO::VBO()
 {
-    // Initialize variables
     buffer_ = 0;
     type_ = GL_ARRAY_BUFFER;
 }
@@ -14,41 +13,29 @@ VBO::~VBO()
 
 void VBO::free()
 {
-    // Delete the buffer
     glDeleteBuffers(1, &buffer_);
-
-    // Clear the data
     data_.clear();
 }
 
 void VBO::create(GLint size)
 {
-    // Create the buffer
     glGenBuffers(1, &buffer_);
-
-    // Set data size
     data_.reserve(size);
 }
 
 void VBO::bind(GLuint bufferType)
 {
-    // Set buffer type
     type_ = bufferType;
-
-    // Bind VBO as current array
     glBindBuffer(type_, buffer_);
 }
 
 void VBO::unbind() const
 {
-    // Unbind VBO as current array
     glBindBuffer(type_, 0);
 }
 
 void VBO::add_data(void* newData, GLuint dataSize)
 {
-    // Insert data at the end of the expandable data vector
-    // cast data to byte pointers
     GLbyte* newDataStart = static_cast<GLbyte*>(newData);
     GLbyte* newDataEnd = static_cast<GLbyte*>(newData) + dataSize;
     data_.insert(data_.end(), newDataStart, newDataEnd);
@@ -56,10 +43,7 @@ void VBO::add_data(void* newData, GLuint dataSize)
 
 void VBO::upload_data(GLenum drawingHint)
 {
-    // Upload the data to the gpu
     glBufferData(type_, data_.size(), data_.data(), drawingHint);
-
-    // Clear the data vector
     data_.clear();
 }
 
--- Source/Core/VBO.h
+++ Source/Core/VBO.h
@@ -3,7 +3,6 @@
 
 #include "GlobalIncludes.h"
 
-// Vertex Buffer Object class
 class VBO
 {
 public:
@@ -11,33 +10,17 @@ public:
     VBO();
     ~VBO();
 
-    // Creates buffer objects
     void create(GLint size = 0);
-
-    // Destroys buffer object
     void free();
-
-    // Binds as current buffer
     void bind(GLuint bufferType = GL_ARRAY_BUFFER);
-
-    // Unbinds as current buffer
     void unbind() const;
-
-    // Adds data
     void add_data(void* newData, GLuint dataSize);
-
-    // Uploads data
     void upload_data(GLenum drawingHint = GL_STATIC_DRAW);
 
 private:
 
-    // The buffer
     GLuint buffer_;
-
-    // Buffer type
     GLuint type_;
-
-    // Expandable data vector
     std::vector<GLbyte> data_;
 };
 
