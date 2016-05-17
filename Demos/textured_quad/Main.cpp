1885826383ab8913d2bb8b7f9ffe901b6e252e28
Matthew J. Berger <matthewberger@nevada.unr.edu>
2016-05-17 12:45:27 -0700

    Correct demo syntax.

--- Demos/textured_quad/Main.cpp
+++ Demos/textured_quad/Main.cpp
@@ -6,7 +6,7 @@ int main()
     std::unique_ptr<iceberg::Game> game = std::make_unique<iceberg::Game>(iceberg::OPENGL);
     std::unique_ptr<ExampleState> Example = std::make_unique<ExampleState>(game.get());
 
-    std::shared_ptr<iceberg::Window> window = game->window_manager()->create_window("Iceberg3D", 640, 480);
+    auto window = game->window_manager()->create_window("Iceberg3D", 640, 480);
     window->set_caption("Iceberg3D Engine - OpenGL Version: " + game->window_manager()->api_version_string());
     window->set_icon("assets/icon.png");
     game->change_state(Example.get());
